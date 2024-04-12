8f0cb8d68f786a7cdec13765e8e0e43fb2266266
inclyner <inclyner@gmail.com>
2024-04-12 22:44:15 +0100

    initial set up

---
+++ src/pt/isec/pa/javalive/model/data/Fauna.java
@@ -0,0 +1,18 @@
+package pt.isec.pa.javalive.model.data;
+
+public non-sealed class Fauna extends ElementoBase {
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Elemento getType() {
+        return null;
+    }
+
+    @Override
+    public Area getArea() {
+        return null;
+    }
+}
---
+++ src/pt/isec/pa/javalive/model/data/Inanimado.java
@@ -0,0 +1,21 @@
+package pt.isec.pa.javalive.model.data;
+
+public non-sealed class Inanimado extends ElementoBase {
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Elemento getType() {
+        return null;
+    }
+
+    @Override
+    public Area getArea() {
+        return null;
+    }
+
+
+
+}
