a9a83944d20a2ff4891f44d8906f076c18eba0a9
inclyner <inclyner@gmail.com>
2024-04-13 00:10:10 +0100

    added basic variables to animal and erva

--- src/pt/isec/pa/javalive/model/data/Animal.java
+++ src/pt/isec/pa/javalive/model/data/Animal.java
@@ -1,7 +1,19 @@
 package pt.isec.pa.javalive.model.data;
 
 public class Animal extends Fauna{
+
+    int velocidade,direcao,forca=50;
+    boolean estadoProcuraComida=false;
     public Animal(int x, int y) {
         super(x, y);
+        direcao= (int) (Math.random()*360);
+    }
+
+    public void movimenta(){
+        forca-=0.5;
+    }
+
+    public void aumentaForca(){
+        forca+=0.5;
     }
 }
--- src/pt/isec/pa/javalive/model/data/Erva.java
+++ src/pt/isec/pa/javalive/model/data/Erva.java
@@ -1,7 +1,15 @@
 package pt.isec.pa.javalive.model.data;
 
 public class Erva extends Flora{
+    double forca=50; //força inicial é 50
+    boolean reproduzivel=false; // quando a forca chega aos 90 a erva pode reproduzir
+    int numdeReproducoes=0; //quantas vezes a erva ja foi reproduzida (max de 2)
     public Erva(int x, int y) {
         super(x, y);
     }
+
+    public void aumentaForca(){
+        forca+=0.5;
+    }
+
 }
