212d8d573e601087e1784152109f3f36b7f7b037
inclyner <inclyner@gmail.com>
2024-04-14 12:09:27 +0100

    multiples changes

--- src/pt/isec/pa/javalife/model/data/Flora.java
+++ src/pt/isec/pa/javalife/model/data/Flora.java
@@ -3,9 +3,12 @@ package pt.isec.pa.javalife.model.data;
 public sealed class Flora extends ElementoBase implements IElementoComForca permits Erva {
 
     boolean reproduzivel=false; // quando a forca chega aos 90 a erva pode reproduzir
-    int numdeReproducoes=0; //quantas vezes a erva ja foi reproduzida (max de 2)
+    int numdeReproducoes=0;//quantas vezes a erva ja foi reproduzida (max de 2)
+    double forca;
     public Flora(int x, int y, int sizeX, int sizeY) {
         super(x, y, sizeX, sizeY);
+        setForca(50);
+
     }
 
     @Override
@@ -33,10 +36,11 @@ public sealed class Flora extends ElementoBase implements IElementoComForca perm
 
     @Override
     public double getForca() {
-        return 0;
+        return forca ;
     }
 
     @Override
     public void setForca(double forca) {
+        this.forca = forca;
     }
 }
