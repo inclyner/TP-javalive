444176b6785aa78a36a3a1be0dfffe3d51650b45
BrunoMartibns <brunodpmartins@gmail.com>
2024-05-29 17:05:45 +0100

    alteraçoes do ecossistema (verificacoes) criacao da funcao evolve no Ecossistema

--- src/pt/isec/pa/javalife/model/data/Ecossistema.java
+++ src/pt/isec/pa/javalife/model/data/Ecossistema.java
@@ -184,7 +184,7 @@ public class Ecossistema implements Serializable, IGameEngineEvolve {
                         fauna.setForca(fauna.getForca() - fauna.getForcaMovimentacao());
                     }
                     if(fauna.reproducao(verificarFaunaDistancia(fauna))){
-                        
+
                     }
                 }
             }
