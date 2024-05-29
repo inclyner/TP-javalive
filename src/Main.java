93aba0c5390f46a07cbc5804e27fd7754632d18e
inclyner <inclyner@gmail.com>
2024-05-29 19:04:55 +0100

    javafx commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 import pt.isec.pa.javalife.model.EcoSistemaFacade.EcosystemFacade;
 import pt.isec.pa.javalife.model.data.Ecossistema;
 import pt.isec.pa.javalife.model.gameengine.GameEngine;
 import pt.isec.pa.javalife.model.gameengine.IGameEngine;
+import pt.isec.pa.javalife.ui.gui.res.MainJFX;
 
-public class Main {
+
+public class Main extends Application {
     public static void main(String[] args) {
 
         IGameEngine gameEngine = new GameEngine();
@@ -14,6 +18,15 @@ public class Main {
         gameEngine.registerClient(ecossistema);
         gameEngine.start(1000);
         gameEngine.waitForTheEnd();
+
+        Application.launch(MainJFX.class, args);
+
+
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
 
 
     }
---
+++ src/pt/isec/pa/javalife/ui/gui/res/MainJFX.java
@@ -0,0 +1,13 @@
+package pt.isec.pa.javalife.ui.gui.res;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainJFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+// TODO
+
+
+    }
+}
